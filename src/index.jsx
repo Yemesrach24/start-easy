db732146631b3956534fda65694fac7569dd4cbf
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2024-12-17 18:01:04 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import reactDom from "react-dom";
 
-ReactDOM.render(
+reactDom.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>NUMBERS</h1>
+    <ul>
+      <li>UNO</li>
+      <li>DUE</li>
+      <li>TRE</li>
+    </ul>
   </div>,
+
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
